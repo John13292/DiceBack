1b8cceedd41b53a7286ce52c6d7483502cddcccb
eugene <bykov.ey@gmail.com>
2022-02-17 17:18:04 +1000

    Перенес картинки на бэк

--- DiceBack/Migrations/20220217064908_AddImageModel.Designer.cs
+++ DiceBack/Migrations/20220217064908_AddImageModel.Designer.cs
@@ -4,6 +4,7 @@ using DiceBack.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace DiceBack.Migrations
 {
     [DbContext(typeof(DiceBackContext))]
-    partial class DiceBackContextModelSnapshot : ModelSnapshot
+    [Migration("20220217064908_AddImageModel")]
+    partial class AddImageModel
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- DiceBack/Models/TestData.cs
+++ DiceBack/Models/TestData.cs
@@ -13,33 +13,58 @@ namespace DiceBack.Data
                 serviceProvider.GetRequiredService<DbContextOptions<DiceBackContext>>())
             )
             {
-                if (context.Effects.Any())
+                if (context.Effects.Any() && context.Images.Any())
                 {
                     return;
                 }
 
-                context.Effects.AddRange(
-                    new Effects
-                    {
-                        Name = "Дикая магия. После каждого хода персонажей кидается д20. Если выпало >10, кидаем таблицу дикой магии",
-                        IsNegative = true
-                    },
-                    new Effects
-                    {
-                        Name = "Больше здоровья у врагов",
-                        IsNegative = true
-                    },
-                    new Effects
-                    {
-                        Name = "У героев д4 легендарных действий",
-                        IsPositive = true
-                    },
-                    new Effects
-                    {
-                        Name = "Больше золота за победу",
-                        IsPositive = true
-                    }
-                );
+                if (!context.Effects.Any())
+                {
+                    context.Effects.AddRange(
+                        new Effects
+                        {
+                            Name = "Дикая магия. После каждого хода персонажей кидается д20. Если выпало >10, кидаем таблицу дикой магии",
+                            IsNegative = true
+                        },
+                        new Effects
+                        {
+                            Name = "Больше здоровья у врагов",
+                            IsNegative = true
+                        },
+                        new Effects
+                        {
+                            Name = "У героев д4 легендарных действий",
+                            IsPositive = true
+                        },
+                        new Effects
+                        {
+                            Name = "Больше золота за победу",
+                            IsPositive = true
+                        }
+                    );
+                }
+
+                if (!context.Images.Any())
+                {
+                    context.Images.AddRange(
+                        new Images
+                        {
+                            Name = "gargoyle",
+                            Url = "https://game-icons.net/icons/ffffff/000000/1x1/delapouite/gargoyle.svg"
+                        },
+                        new Images
+                        {
+                            Name = "minotaur",
+                            Url = "https://game-icons.net/icons/ffffff/000000/1x1/lorc/minotaur.svg"
+                        },
+                        new Images
+                        {
+                            Name = "dragon-head",
+                            Url = "https://game-icons.net/icons/ffffff/000000/1x1/faithtoken/dragon-head.svg"
+                        }
+                    );
+                }
+                
 
                 context.SaveChanges();
             }
