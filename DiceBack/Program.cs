89ce245742de88fe9425909252bc9a806bb544e9
eugene <bykov.ey@gmail.com>
2022-08-11 23:59:08 +1000

    Перенес настройки cors, automapper в экстйшены

--- DiceBack/Program.cs
+++ DiceBack/Program.cs
@@ -1,7 +1,7 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using DiceBack.DataBase;
 using DiceBack.Application.Extensions;
-using DiceBack.Application.Mapping;
+using DiceBack.Api.System.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,42 +11,25 @@ builder.Services.AddDbContext<DiceBackContext>(options =>
     )
 );
 
-// Add services to the container.
-
+//add system
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddAutoMapper(
-    confug => { confug.AllowNullCollections = true; },
-    typeof(EffectDtoMappingEffect),
-    typeof(ImageDtoMappingImage)
-    );
+builder.Services.AutoMapperConfiguration();
+builder.Services.CorsConfiguration();
 
-
+//add bussines 
 builder.Services.AddImageIntegration();
 builder.Services.AddEffectIntegration();
 builder.Services.AddEffectGeneratorIntegration();
 
-builder.Services.AddCors(optioins =>
-    {
-        optioins.AddPolicy("FrontConnection", builder =>
-        {
-            builder
-                .WithOrigins("http://localhost:8080")
-                .AllowAnyHeader()
-                .AllowAnyMethod();
-        });
-    }
-);
+
 
 var app = builder.Build();
 
 using (var scope = app.Services.CreateScope())
 {
     var service = scope.ServiceProvider;
-
-    //TestData.Initialize(service);
 }
 
 // Configure the HTTP request pipeline.
---
+++ DiceBack/System/Extensions/AutoMapperExtensions.cs
@@ -0,0 +1,18 @@
+﻿using DiceBack.Application.Mapping;
+
+namespace DiceBack.Api.System.Extensions
+{
+    public static class AutoMapperExtensions
+    {
+        public static IServiceCollection AutoMapperConfiguration(this IServiceCollection services)
+        {
+            services.AddAutoMapper(
+                confug => { confug.AllowNullCollections = true; },
+                typeof(EffectDtoMappingEffect),
+                typeof(ImageDtoMappingImage)
+            );
+
+            return services;
+        }
+    }
+}
